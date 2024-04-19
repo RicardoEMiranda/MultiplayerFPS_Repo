0e5c35fda1b9931e844674a420fb1d32fafe50d6
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2024-04-19 15:35:10 -0400

    Player Controller (ViewPoint Control)
    Added camera viewpoint controls based on horizontal and vertical mouse movements.

--- Multiplayer FPS/Assets/Scripts/PlayerController.cs
+++ Multiplayer FPS/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+
+    public Transform viewPoint;
+    public float mouseSensitivity = 2f;
+    private Vector2 mouseInput;
+    private float eulerX;
+    private float eulerY;
+    private float eulerZ;
+    private float viewPointVerticalRotation;
+    private float vertRotationStore;
+
+
     // Start is called before the first frame update
     void Start() {
-        
+        //eulerX = transform.rotation.eulerAngles.x;
+        eulerX = 0f;
+        eulerZ = 0f;
+        vertRotationStore = 0f;
     }
 
     // Update is called once per frame
     void Update() {
-        
+        //Debug.Log("Mouse Input X: " + Input.GetAxisRaw("Mouse X"));
+
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+        eulerY = transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(eulerX, eulerY + mouseInput.x, eulerZ);
+
+        //viewPointVerticalRotation = viewPoint.rotation.eulerAngles.x - mouseInput.y;
+        //viewPoint.rotation = Quaternion.Euler(viewPointVerticalRotation, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        vertRotationStore = vertRotationStore - mouseInput.y;
+        vertRotationStore = Mathf.Clamp(vertRotationStore, -30f, 30f);
+        viewPoint.rotation = Quaternion.Euler(vertRotationStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
     }
 }
